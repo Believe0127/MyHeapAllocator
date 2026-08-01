327cb8431958faa604483ce3655f5c7d9509c1cc
believe0127 <ringo10270620@gmail.com>
2026-08-01 16:46:49 +0900

    minute changes

--- include/Heap.hpp
+++ include/Heap.hpp
@@ -7,20 +7,21 @@
 
 class HeapAllocator;
 class Heap {
-    struct alignas(16) HeapHeader {
+    static constexpr size_t alignment = 16;
+
+    struct alignas(alignment) HeapHeader {
         size_t capacity;
         size_t used_size; // 使用済みのサイズ (HeapHeader/BlockHeader を含む)
     };
-    static_assert(sizeof(HeapHeader) == 16);
+    static_assert(sizeof(HeapHeader) == alignment);
 
-    struct alignas(16) BlockHeader {
+    struct alignas(alignment) BlockHeader {
         u32          block_size;
         u32          is_free;
         BlockHeader* prev;
     };
-    static_assert(sizeof(BlockHeader) == 16);
+    static_assert(sizeof(BlockHeader) == alignment);
 
-    static constexpr size_t alignment          = 16;
     static constexpr size_t minimum_block_size = sizeof(BlockHeader) + alignment;
 public:
     Heap(size_t heap_size);
